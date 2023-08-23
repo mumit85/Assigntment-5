48048a306ffedaa4f04d81441a9b9cb869e88dac
Mumit Ibna Idris <102920681+mumit85@users.noreply.github.com>
2023-08-23 19:49:14 +0600

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+    if(isset($_GET["id"])){
+        $id = $_GET["id"];
+
+        $servername = "localhost";
+        $pass = "";
+        $username = "root";
+        $db = "to-do-list";
+
+        $conn = new mysqli($servername, $username, $pass, $db);
+
+        $sql = "DELETE FROM todo WHERE sr_no = $id";
+        $conn -> query($sql);
+    }
+
+    header("location: ./index.php");
+    exit;
+    
+?>
